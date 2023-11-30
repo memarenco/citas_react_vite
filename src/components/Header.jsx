685fb3b15cbc335ab5e05ffc59285d4514072bbf
Melvin Marenco <memarenco@gmail.com>
2023-11-29 20:52:42 -0600

    Se agrega el nombre de react.js al header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@ function Header() {
     <>
       <h1 className="font-black text-5xl text-center md:w-2/3 mx-auto">
         Seguimiento de Pacientes{" "}
-        <span className="text-indigo-600">Veterinaria</span>
+        <span className="text-indigo-600">Veterinaria con React.js</span>
       </h1>
     </>
   );
